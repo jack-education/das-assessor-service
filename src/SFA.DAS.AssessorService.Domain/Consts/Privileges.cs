f289e15564f75f7b5b8dbcd3ab16169478827916
ArshedMust <arshed@air2wire.com>
2019-07-22 15:46:04 +0100

    Redone some more changes

--- src/SFA.DAS.AssessorService.Domain/Consts/Privileges.cs
+++ src/SFA.DAS.AssessorService.Domain/Consts/Privileges.cs
@@ -4,11 +4,11 @@ namespace SFA.DAS.AssessorService.Domain.Consts
     public static class Privileges
     {
         // This MUST be kept in line with dbo.Privileges
-        public const string ViewCompletedAssessments = "View Completed assessments";
+        public const string ViewCompletedAssessments = "View completed assessments";
         public const string ApplyForStandard = "Apply for a Standard";
         public const string ManageAPISubscription = "Manage API subscription";
         public const string RecordGrades = "Record grades and issue certificates";
         public const string ManageUsers = "Manage users";
-        public const string ViewPipeline = "View Pipeline";
+        public const string ViewPipeline = "View pipeline";
     }
 }
